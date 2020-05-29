6d0dbc65456750c44e140bdf6c8f15d1fe5d972b
Huhsway <seunghoe9542@naver.com>
2020-05-29 17:09:02 +0900

    output.json 파일 만들기

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require('fs');
 const log = console.log;
 
 const getHtml = async () => {
@@ -21,15 +22,14 @@ getHtml()
       ulList[i] = {
           title: $(this).find('h1.nm-collections-row-name a.nm-collections-link span.nm-collections-row-name').text(),
           url: $(this).find('div.nm-content-horizontal-row ul.nm-content-horizontal-row-item-container li.nm-content-horizontal-row-item a.nm-collections-link').attr('href'),
-          
-          //image_url: $(this).find('p.poto a img').attr('src'),
-          //image_alt: $(this).find('p.poto a img').attr('alt'),
-          //summary: $(this).find('p.lead').text().slice(0, -11),
-          //date: $(this).find('span.p-time').text()
       };
     });
 
     const data = ulList.filter(n => n.title);
+    let output = JSON.stringify(data);
+    fs.writeFileSync('output.json', output);
     return data;
   })
   .then(res => log(res));
+
+  
